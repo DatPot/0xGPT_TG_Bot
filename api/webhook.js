03730354bbb646b19dad3055fecffaaed44f135a
DatPot <139276232+DatPot@users.noreply.github.com>
2023-09-08 20:17:10 -0700

    Update webhook.js
    Command to set up Telegram Webhook
    
    Not sure if I install npm install node-telegram-bot-api

---
+++ api/webhook.js
@@ -0,0 +1,4 @@
+// sets up Telegram webhook
+curl -X POST //api.telegram.org/bot<YOUR_HTTP_API_TOKEN>/setWebhook?url=<YOUR_API_GATEWAY_URL>
+
+// Path: 0xGPT_TG_Bot/api/webhook.js (Need to install Telegram bot API as well?)
